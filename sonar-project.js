3f64bdf51426bda0d010658f7f6f132dac6981a6
vishnuvardhanreddy512 <47027676+vishnuvardhanreddy512@users.noreply.github.com>
2022-05-02 14:10:40 +0530

    sonar details updated

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.255.130:9000',
+  serverUrl: 'http://13.232.170.115:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '219a0309ba48ec3900f263329629970429e31997',
+	    'sonar.login': '783959ed-cbaf-4d10-8b3b-786e7c5a19b2',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
